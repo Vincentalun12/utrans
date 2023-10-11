d16f92949733bdef7477148bbbe7b3a7b074b3c7
Alun <vengalolyeah@gmail.com>
2023-10-11 13:02:05 +0700

    Laravel breeze

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [laravel(["resources/js/app.jsx"]), react()],
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
