5b69ac8600951534682e87f0388f0fd58154a9bb
Hernando17 <ginkotsu17@gmail.com>
2023-10-13 23:13:04 +0700

    images folder

---
+++ resources/js/Assets/image/index.jsx
@@ -0,0 +1,3 @@
+import CashierVector from "./Cashier_Vector.png";
+
+export { CashierVector };
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,16 +1,19 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
+import { CashierVector } from '@/Assets';
+
 export default function Guest({ children }) {
-return (
-<div className="h-screen md:flex">
-  <div
-    className="relative overflow-hidden bg-ungukita md:flex w-1/2 justify-around items-center hidden">
-  </div>
-  <div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
-    <div className="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
-      {children}  
+  return (
+    <div className="h-screen md:flex">
+      <div
+        className="relative overflow-hidden bg-ungukita md:flex w-1/2 justify-around items-center hidden">
+        <img src={CashierVector} alt="Cashier Vector" className="w-1/2" />
+      </div>
+      <div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
+        <div className="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+          {children}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 }
