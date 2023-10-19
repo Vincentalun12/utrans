bdb985c8f5ee52cbb26a3ecbe60912f1b68de2e2
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-10-19 20:51:20 +0700

    Updated user profile menu

--- resources/js/Layouts/NavigationLayout.jsx
+++ resources/js/Layouts/NavigationLayout.jsx
@@ -25,7 +25,8 @@ import {
   Menu,
   MenuHandler,
   MenuList,
-  MenuItem
+  MenuItem,
+  MenuHandlerProps
 } from "@material-tailwind/react";
 
 import {
@@ -237,7 +238,7 @@ export default function Authenticated({ user, header, children }) {
                     >
                     <Menu
                     placement="bottom-end"
-                    //allowHover={true}  Kalo mau allow hover
+                    open={true}
                     animate={{mount: { y: 0 },unmount: { y: -5 },}}>
                       <MenuHandler>
                       <span className="inline-flex rounded-md">
@@ -260,7 +261,7 @@ export default function Authenticated({ user, header, children }) {
                           viewBox="0 0 24 24"
                           strokeWidth="1.5"
                           stroke="currentColor"
-                          class="w-6 h-6"
+                          className="w-6 h-6"
                         >
                           <path
                             strokeLinecap="round"
@@ -283,10 +284,15 @@ export default function Authenticated({ user, header, children }) {
                         </Typography>
                       </MenuItem>
                       <hr className="my-2 border-blue-gray-50" />
-                      <MenuItem className="flex items-center gap-2 bg-red-500">
+                      <MenuItem 
+                        href={route("logout")}
+                        method="post"
+                        as="button" 
+                        className="flex items-center gap-2 bg-red-500">
+                        
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff" className="w-6 h-6">
                         <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clipRule="evenodd" /></svg>
-                        <Typography variant="small" className="font-bold text-white">
+                        <Typography variant="small" className="font-bold text-white" >
                           Log Out
                         </Typography>
                       </MenuItem>
