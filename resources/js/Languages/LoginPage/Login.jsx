8775f558ee0688a9d75cc59f2ff4fc12dc0a9178
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-10-25 00:22:56 +0700

    Fix typo

--- resources/js/Languages/LoginPage/Login.jsx
+++ resources/js/Languages/LoginPage/Login.jsx
@@ -3,14 +3,14 @@ import LocalizedStrings from "react-localization";
 let Language = new LocalizedStrings({
     id: {
         login_id: "Masuk",
-        email_id: "Email",
+        //email_id: "Email",
         password_id: "Kata Sandi",
-        remember_id: "Ingat Saya",
+        remember_id: "Ingat saya",
         forgot_id: "Lupa kata sandi?",
     },
     en: {
         login_en: "Login",
-        email_en: "Email",
+        //email_en: "Email",
         password_en: "Password",
         remember_en: "Remember me",
         forgot_en: "Forgot your password?",
