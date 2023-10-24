f88c29cc0816fb60a4a49adbb779d770f9c3a9e0
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-10-25 00:31:09 +0700

    Full Language support on Login Page

--- resources/js/Languages/LoginPage/Login.jsx
+++ resources/js/Languages/LoginPage/Login.jsx
@@ -7,6 +7,9 @@ let Language = new LocalizedStrings({
         password_id: "Kata Sandi",
         remember_id: "Ingat saya",
         forgot_id: "Lupa kata sandi?",
+        emailfield_id: "Alamat email dibutuhkan di kolom ini.",
+        passwordfield_id: "Kata sandi dibutuhkan di kolom ini.",
+        loginfail_id: "Alamat email atau kata sandi tidak cocok dengan data kami."
     },
     en: {
         login_en: "Login",
@@ -14,6 +17,9 @@ let Language = new LocalizedStrings({
         password_en: "Password",
         remember_en: "Remember me",
         forgot_en: "Forgot your password?",
+        emailfield_id: "The email field is required.",
+        passwordfield_id: "The password field is required.",
+        loginfail_id: "These credentials do not match our records."
     },
 });
 
