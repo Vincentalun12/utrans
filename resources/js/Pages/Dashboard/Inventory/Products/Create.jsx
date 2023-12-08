1404ca8c3ac8c591cb846d5dfc27eafa51605edd
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-12-08 19:35:57 +0700

    Update NPM + Fixed input with material tailwind

--- resources/js/Pages/Dashboard/Inventory/Products/Create.jsx
+++ resources/js/Pages/Dashboard/Inventory/Products/Create.jsx
@@ -8,6 +8,8 @@ import {
   Breadcrumbs,
   Select,
   Option,
+  Label,
+  Textarea
 } from "@material-tailwind/react";
 
 import {
@@ -42,22 +44,18 @@ export default function Additem({ auth }) {
             <div className="grid lg:gap-8 grid-cols-1 gap-4 p-4 mx-4 mt-5">
               <div>
                 <div className="p-2 text-black font-bold text-xl mb-2">Name and brands</div>
-                <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
-                  <label className="">Name</label>
-                  <input
-                    className="w-full block px-0 mt-1 border-none focus:outline-none focus:ring-0"
-                    placeholder="Input your product name."
-                  />
-                </div>
-                <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                  *Provide the name of your product that is suitable for the
+                  <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
+                    <Input variant="static" label="Name" className='border-none focus:shadow-none' placeholder="Input your product name" />
+                  </div>
+                  <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
+                  *Provide the name of your brands that is suitable for the
                   item you are selling.
                 </div>
-              </div>
+                </div>
               <div>
                 <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
                   <label className="">Brand</label>
-                  <Select variant="static">
+                  <Select variant="static" placeholder="Select brand from the following..">
                   <Option>1</Option>
                   <Option>2</Option>
                   <Option>3</Option>
@@ -73,33 +71,24 @@ export default function Additem({ auth }) {
             <div>
               <div className="p-2 text-black font-bold text-xl mb-2">Prices and stock</div>
               <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
-                <label className="">Retail</label>
-                <input className="w-full block px-0 mt-1 border-none focus:outline-none focus:ring-0"
-                       placeholder="Input your retail price."
-                      />
-              </div>
+                    <Input variant="static" label="Retail" className='border-none focus:shadow-none' placeholder="Input your retail price" />
+                  </div>
               <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
                   *For each items.
                 </div>
             </div>
             <div>
               <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
-                <label className="">Wholesale</label>
-                <input className="w-full block px-0 mt-1 border-none focus:outline-none focus:ring-0"
-                placeholder="Input your wholesale price."
-                />
-              </div>
+                    <Input variant="static" label="Wholesale" className='border-none focus:shadow-none' placeholder="Input your wholesale price" />
+                </div>
               <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
                   *For each items.
                 </div>
               </div>
               <div>
               <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
-                <label className="">Stock</label>
-                <input className="w-full block px-0 mt-1 border-none focus:outline-none focus:ring-0"
-                placeholder="Input your stocks."
-                />
-              </div>
+                    <Input variant="static" label="Stocks" className='border-none focus:shadow-none' placeholder="Input your stocks" />
+                  </div>
               <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
                   *Put your stock x1.
                 </div>
