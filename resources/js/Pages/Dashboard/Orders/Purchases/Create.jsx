350b35c936df36e2c773f23b786352ab0045a549
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-12-12 16:20:56 +0700

    Implemented date time to arrival-date

--- resources/js/Pages/Dashboard/Orders/Purchases/Create.jsx
+++ resources/js/Pages/Dashboard/Orders/Purchases/Create.jsx
@@ -3,6 +3,8 @@ import Linkactive from "@/Components/Linkactive";
 import React from 'react'
 import Select from 'react-select'
 import { Head } from "@inertiajs/react";
+import { format, parse } from "date-fns";
+import { DayPicker } from "react-day-picker";
 import {
   Card,
   CardHeader,
@@ -15,6 +17,9 @@ import {
   Tabs,
   TabsHeader,
   Tab,
+  Popover,
+  PopoverContent,
+  PopoverHandler,
   Avatar,
   IconButton,
   Tooltip,
@@ -37,6 +42,8 @@ import {
   CalendarDaysIcon,
   QrCodeIcon,
   TrashIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
   PlusIcon,
 } from "@heroicons/react/24/solid";
 
@@ -86,6 +93,8 @@ const options = [
 
 
 export default function Additem({ auth }) {
+  const [date, setDate] = React.useState(new Date());
+
   const style = {
     control: base => ({
       ...base,
@@ -139,30 +148,83 @@ export default function Additem({ auth }) {
                 </div>
                 </div>
                 <div className="lg:mx-2 lg:w-4/6 w-full py-2 lg:py-0">
-              <label className="">Creation date</label>
-              <Input
-                  type="search"
-                  placeholder="12/5/2023"
-                  icon={<CalendarDaysIcon/>}
-
-                  className="  placeholder:text-ungukita focus:!border-ungukita focus:ring-ungukita"
-                  labelProps={{
-                    className: "before:content-none after:content-none",
-                  }}
-                />
+                <label className="">Creation Date</label>
+                  <Input
+                      type="search"
+                      placeholder="14-5-2023"
+                      disabled
+                      className="  placeholder:text-ungukita focus:!border-ungukita focus:ring-ungukita"
+                      labelProps={{
+                        className: "before:content-none after:content-none",
+                      }}
+                    />
                 </div>
                 <div className="lg:mx-2 lg:w-4/6 w-full py-2 lg:py-0">
-              <label className="">Expected Arrival</label>
-              <Input
-                  type="search"
-                  placeholder="14/5/2023"
-                  icon={<CalendarDaysIcon/>}
-
-                  className="  placeholder:text-ungukita focus:!border-ungukita focus:ring-ungukita"
-                  labelProps={{
-                    className: "before:content-none after:content-none",
-                  }}
-                />
+                <label className="">Arrival Date</label>
+                    <Popover placement="bottom" trigger="click">
+                        <PopoverHandler>
+                        <Input
+                          type="text"
+                          placeholder="2023-05-12"
+                          icon={<CalendarDaysIcon/>}
+                          value={format(date, 'dd-MM-yyyy')}
+                          onChange={(e) => {
+                            const newDate = parse(e.target.value, "dd-MM-yyyy", new Date());
+                            if (!isNaN(newDate)) {
+                              setDate(newDate);
+                            }
+                          }}
+                          className="  placeholder:text-ungukita focus:!border-ungukita focus:ring-ungukita"
+                          labelProps={{
+                            className: "before:content-none after:content-none",
+                          }}
+                        />
+                        </PopoverHandler>
+                        <PopoverContent>
+                        <DayPicker
+                            mode="single"
+                            selected={date}
+                            onSelect={(selectedDate) => {
+                              if (selectedDate) {
+                                setDate(selectedDate);
+                              }
+                            }}
+                            showOutsideDays
+                            className="border-0"
+                            classNames={{
+                              caption: "flex justify-center py-2 mb-4 relative items-center",
+                              caption_label: "text-sm font-medium text-gray-900",
+                              nav: "flex items-center",
+                              nav_button:
+                                "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
+                              nav_button_previous: "absolute left-1.5",
+                              nav_button_next: "absolute right-1.5",
+                              table: "w-full border-collapse",
+                              head_row: "flex font-medium text-gray-900",
+                              head_cell: "m-0.5 w-9 font-normal text-sm",
+                              row: "flex w-full mt-2",
+                              cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+                              day: "h-9 w-9 p-0 font-normal",
+                              day_range_end: "day-range-end",
+                              day_selected:
+                                "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
+                              day_today: "rounded-md bg-gray-200 text-gray-900",
+                              day_outside:
+                                "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
+                              day_disabled: "text-gray-500 opacity-50",
+                              day_hidden: "invisible",
+                            }}
+                            components={{
+                              IconLeft: ({ ...props }) => (
+                                <ChevronLeftIcon {...props} className="h-4 w-4 stroke-2" />
+                              ),
+                              IconRight: ({ ...props }) => (
+                                <ChevronRightIcon {...props} className="h-4 w-4 stroke-2" />
+                              ),
+                            }}
+                          />
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <div className="lg:mx-2 lg:w-4/6 w-full pt-2 lg:py-0">
               <label className="">Reference</label>
