c5ea06d56e6899f5a6a342ec616e3fd1d17c4909
jupitvq <40255470+jupitvq@users.noreply.github.com>
2023-12-15 17:27:11 +0700

    Implemented new UI layout, currently applied to Products (Currently buggy when editing data. rasis jir)

--- resources/js/Pages/Dashboard/Inventory/Products/Create.jsx
+++ resources/js/Pages/Dashboard/Inventory/Products/Create.jsx
@@ -1,12 +1,12 @@
 import AdditemLayout from "@/Layouts/NavigationLayout";
 import { Head, useForm } from "@inertiajs/react";
+import Select from 'react-select'
 import {
     Card,
     Typography,
     Input,
     Button,
     Breadcrumbs,
-    Select,
     Option,
     Label,
     Textarea,
@@ -42,130 +42,163 @@ export default function AddProduct({ auth, brands }) {
             <Head title="Add Product" />
       <div className="sm:mt-18 sm:mb-20 mt-4 mb-0 justify-center ml-0 lg:ml-[300px] sm:mr-1">
                 <div className="mx-auto px-4 sm:px-6 lg:px-6 w-full sm:mt-28">
-                    <Card className="h-full w-full overflow-hidden rounded-none">
+                <div className="w-full mx-auto pb-5">
+                    <div className="bg-white overflow-hidden shadow-sm rounded-lg sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                        <Typography variant="h4" className="text-ungukita" textGradient>
+                            Create Products
+                        </Typography>
+                        <Typography variant="paragraph">
+                            Create your Products to be sold on your store
+                        </Typography>
+                        </div>
+                    </div>
+                </div>
+
+                    <Card className="h-full w-full rounded-md overflow-hidden pb-4">
+                    <form onSubmit={actionSubmit}>
                         <div className="grid lg:gap-8 grid-cols-1 gap-4 p-4 mx-4 mt-5">
-                            <form onSubmit={actionSubmit}>
-                                <div>
-                                    <div className="p-2 text-black font-bold text-xl mb-2">
-                                        Name and brands
+                                <div className="sm:col-span-1">
+                                    <Typography>Product Name</Typography>
+                                    <div className="w-full text-xs mb-2 text-gray-500">
+                                        *Provide the name of your brands that is
+                                        suitable for the item you are selling.
                                     </div>
-                                    <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
                                         <Input
-                                            variant="static"
-                                            label="Name"
-                                            name="name"
-                                            className="border-none focus:shadow-none"
-                                            placeholder="Input your product name"
+                                            type="input"
+                                            placeholder="Name"
                                             value={data.name}
                                             onChange={(e) => {
                                                 setData("name", e.target.value);
                                             }}
-                                        />
-                                    </div>
-                                    <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                                        *Provide the name of your brands that is
-                                        suitable for the item you are selling.
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
-                                        <label className="">Brand</label>
-                                        <Select
-                                            name="brand_id"
-                                            value={data.brand_id}
-                                            onChange={(e) => {
-                                                setData(
-                                                    "brand_id",
-                                                    e.target.value
-                                                );
+                                            className="  placeholder:text-gray-600 placeholder:opacity-100 !border-t-blue-gray-200 focus:!border-ungukita focus:ring-ungukita"
+                                            labelProps={{
+                                                className: "before:content-none after:content-none",
                                             }}
-                                            variant="static"
-                                            placeholder="Select brand from the following.."
-                                        >
-                                            {brands.map(
-                                                ({ id, name }, index) => (
-                                                    <Option key={id} value={id}>
-                                                        {name}
-                                                    </Option>
-                                                )
-                                            )}
-                                        </Select>
+                                        />
+                                </div>
+                                <div className="sm:col-span-1">
+                                <Typography>Brand</Typography>
+                                <div className="w-full text-xs mb-2 text-gray-500">
+                                        *Pick up the brand of your product, if you haven't added any brand, you can add it <a href="/brand/" className="text-blue-500">here</a>
                                     </div>
-                                    <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                                        *Provide the name of your brands that is
-                                        suitable for the item you are selling.
-                                    </div>
+                                <Select 
+                                        value={brands.find(brand => brand.id === data.brand_id) ? { value: data.brand_id, label: brands.find(brand => brand.id === data.brand_id).name } : null}
+                                        onChange={(selectedOption) => {
+                                            setData("brand_id", selectedOption.value);
+                                          }}
+                                        options={brands.map(brand => ({ value: brand.id, label: brand.name }))}
+                                        placeholder={'Select brands from the following..'}
+                                        styles={{
+                                            control: (base, state) => ({
+                                            ...base,
+                                            boxShadow: state.isFocused ? 0 : 0,
+                                            borderColor: state.isFocused ? '#1A202C' : base.borderColor,
+                                            borderWidth: state.isFocused ? '2px' : '1px',
+                                            "&:hover": {
+                                                borderColor: state.isFocused ? '#1A202C' : base.borderColor
+                                            },
+                                            borderRadius: '6px',
+                                            }),
+                                            input: (base) => ({
+                                            ...base,
+                                            "input:focus": {
+                                                boxShadow: "none",
+                                            },
+                                            })
+                                        }}
+                                    />
                                 </div>
                                 <div>
-                                    <div className="mx-2 border-b lg:w-4/6 w-full">
-                                        <Textarea
-                                            name="description"
+                                    <div className="sm:col-span-1">
+                                    <Typography>Description</Typography>
+                                    <div className="w-full text-xs mb-2 text-gray-500">
+                                        *Provide the description or notes you want to added on the product
+                                    </div>
+                                    <Textarea
+                                            type="input"
+                                            size="md"
+                                            placeholder="Description"
                                             value={data.description}
                                             onChange={(e) => {
-                                                setData(
-                                                    "description",
-                                                    e.target.value
-                                                );
+                                                setData("description", e.target.value);
                                             }}
-                                            variant="static"
-                                            label="Description"
-                                            className="border-none focus:shadow-none"
-                                            placeholder="Input your retail price"
+                                            className="  placeholder:text-gray-600 placeholder:opacity-100 !border-t-blue-gray-200 focus:!border-ungukita focus:ring-ungukita"
+                                            labelProps={{
+                                                className: "before:content-none after:content-none",
+                                            }}
                                         />
                                     </div>
-                                    <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                                        *Provide the Description of your
-                                        products that is suitable for the item
-                                        you are selling.
-                                    </div>
                                 </div>
-                                <div>
-                                    <div className="p-2 text-black font-bold text-xl mb-2">
-                                        Prices
+                                <div className="grid sm:grid-cols-2 gap-4">
+                                <div className="sm:col-span-1">
+                                    <Typography>Retail Price</Typography>
+                                    <div className="w-full text-xs mb-2 text-gray-500">
+                                        * Input your Retail Price
                                     </div>
-                                    <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
+                                    <div className="flex">
+                                        <Button
+                                        ripple={false}
+                                        variant="text"
+                                        color="blue-gray"
+                                        className="normal-case text-bold h-10 flex items-center rounded-r-none border border-r-0 border-blue-gray-200 bg-blue-gray-500/10 px-3"
+                                        >
+                                        Rp
+                                        </Button>
+                                        <div className="relative flex-grow">
                                         <Input
-                                            variant="static"
-                                            label="Retail"
                                             type="number"
-                                            name="retail_price"
                                             value={data.retail_price}
                                             onChange={(e) => {
-                                                setData(
-                                                    "retail_price",
-                                                    e.target.value
-                                                );
+                                            setData("retail_price", e.target.value);
                                             }}
-                                            className="border-none focus:shadow-none"
-                                            placeholder="Input your retail price"
+                                            className="placeholder:text-gray-600 rounded-tl-none rounded-bl-none placeholder:opacity-100 !border-t-blue-gray-200 focus:!border-ungukita focus:ring-ungukita"
+                                            labelProps={{
+                                            className: "before:content-none after:content-none",
+                                            }}
                                         />
-                                    </div>
-                                    <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                                        *For each items.
+                                        </div>
                                     </div>
                                 </div>
-                                <div>
-                                    <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
+                                <div className="sm:col-span-1">
+                                    <Typography>Wholesale Price</Typography>
+                                    <div className=" w-full text-xs mb-2 text-gray-500">
+                                        * Must be greater than retail price
+                                    </div>
+                                    <div className="flex">
+                                        <Button
+                                        ripple={false}
+                                        variant="text"
+                                        color="blue-gray"
+                                        className="normal-case text-bold h-10 flex items-center rounded-r-none border border-r-0 border-blue-gray-200 bg-blue-gray-500/10 px-3"
+                                        >
+                                        Rp
+                                        </Button>
+                                        <div className="relative flex-grow">
                                         <Input
-                                            variant="static"
                                             type="number"
-                                            name="whole_sale_price"
                                             value={data.whole_sale_price}
                                             onChange={(e) => {
-                                                setData(
-                                                    "whole_sale_price",
-                                                    e.target.value
-                                                );
+                                            setData("whole_sale_price", e.target.value);
                                             }}
-                                            label="Whole Sale"
-                                            className="border-none focus:shadow-none"
-                                            placeholder="Input your whole sale price"
+                                            className="placeholder:text-gray-600 rounded-tl-none rounded-bl-none placeholder:opacity-100 !border-t-blue-gray-200 focus:!border-ungukita focus:ring-ungukita"
+                                            labelProps={{
+                                            className: "before:content-none after:content-none",
+                                            }}
                                         />
-                                    </div>
-                                    <div className="lg:w-4/6 w-full text-xs p-3 text-gray-500">
-                                        *For each items.
+                                        </div>
                                     </div>
                                 </div>
+                                <div className="sm:col-span-2 pt-8">
+                                    <Button
+                                        fullWidth
+                                        type="submit"
+                                        className="bg-ungukita"
+                                    >
+                                        Save
+                                    </Button>
+                            </div>  
+                            </div>
 
                                 {/* <div>
                                 <div className="mx-2 border-b lg:w-4/6 w-full border-black hover:border-b-2">
@@ -200,17 +233,9 @@ export default function AddProduct({ auth, brands }) {
                                     *SVG, PNG, JPG or GIF (MAX. 300x300px or 1:1
                                     ratio)
                                 </div>
-                            </div> */}
-                                <div>
-                                    <Button
-                                        type="submit"
-                                        className="w-full lg:w-4/6 h-12 bg-ungukita"
-                                    >
-                                        Save
-                                    </Button>
-                                </div>
-                            </form>
+                            </div> */}                        
                         </div>
+                        </form>
                     </Card>
                 </div>
             </div>
