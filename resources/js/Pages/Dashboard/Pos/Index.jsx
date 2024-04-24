e3f4ca1aa8fda2b1194923cb10d4f58af5c98e43
Vincentalun12 <vengalolyeah@gmail.com>
2024-04-24 17:44:08 +0700

    sa

--- resources/js/Pages/Dashboard/Pos/Index.jsx
+++ resources/js/Pages/Dashboard/Pos/Index.jsx
@@ -23,133 +23,129 @@ DocumentTextIcon,
 } from "@heroicons/react/24/solid";
 import { twMerge } from "tailwind-merge";
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Avatar,
-  Tooltip,
+Card,
+CardHeader,
+CardBody,
+CardFooter,
+Typography,
+Avatar,
+Tooltip,
 } from "@material-tailwind/react";
 export default function Dashboard({ auth }) {
-
-    return (
-        <div className="flex h-screen bg-gray-200">
-        <div className="w-20 bg-white p-3">
-            <div className="mb-4">
-                <i className="fas fa-home"></i>
-            </div>
-            <div className="mb-4">
-                <i className="fas fa-shopping-cart"></i>
-            </div>
-            <div className="mb-4">
-                <i className="fas fa-cash-register"></i>
-            </div>
-        </div>
-
-        <div className="flex-1 p-10 m-2 text-2xl font-bold overflow-auto">
-            <div className="mb-4">
-                <input className="px-3 py-2 border border-gray-400" type="text" placeholder="Search" />
-            </div>
-            <div className="grid xl:grid-cols-3 2xl:grid-cols-6 gap-4">
-                <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        
-        </a>
-			<div class="px-5 pb-5">
+return (
+<div className="flex h-screen bg-gray-200">
+	<div className="w-20 bg-white p-3">
+		<div className="mb-4">
+			<i className="fas fa-home"></i>
+		</div>
+		<div className="mb-4">
+			<i className="fas fa-shopping-cart"></i>
+		</div>
+		<div className="mb-4">
+			<i className="fas fa-cash-register"></i>
+		</div>
+	</div>
+	<div className="flex-1 p-10 m-2 text-2xl font-bold overflow-auto">
+		<div className="mb-4">
+			<input className="px-3 py-2 border border-gray-400" type="text" placeholder="Search" />
+		</div>
+		<div className="grid xl:grid-cols-3 2xl:grid-cols-6 gap-4">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
+			<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
 				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
+				<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
 				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+				<div class="px-5 pb-5">
+					<a href="#">
+						<h3 class="text-gray-900 font-light text-[18px] tracking-tight dark:text-white">Keran 2G22</h3>
+					</a>
+					<div class="flex items-center justify-between">
+						<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+						<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+					</div>
 				</div>
 			</div>
 		</div>
