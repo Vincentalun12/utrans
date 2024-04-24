a494b68d15129b46c63cf33932a8b5f744ec3e54
jupitvq <40255470+jupitvq@users.noreply.github.com>
2024-04-24 18:42:10 +0700

    🔥 Add search functionality, UI elements, and update colors

--- resources/js/Pages/Dashboard/Pos/Index.jsx
+++ resources/js/Pages/Dashboard/Pos/Index.jsx
@@ -42,21 +42,21 @@ export default function Dashboard({ auth }) {
 
     return (
         <div className="flex h-screen bg-gray-200">
-        <div className="w-20 bg-white p-3">
-            <div className="mb-4">
-                <i className="fas fa-home"></i>
-            </div>
-            <div className="mb-4">
-                <i className="fas fa-shopping-cart"></i>
-            </div>
-            <div className="mb-4">
-                <i className="fas fa-cash-register"></i>
-            </div>
-        </div>
+			<div className="w-20 bg-white p-3">
+				<div className="mb-4">
+					<i className="fas fa-home"></i>
+				</div>
+				<div className="mb-4">
+					<i className="fas fa-shopping-cart"></i>
+				</div>
+				<div className="mb-4">
+					<i className="fas fa-cash-register"></i>
+				</div>
+			</div>
 
-        <div className="flex-1 p-10 m-2 text-2xl font-bold overflow-auto">
-			<div className="flex mb-4">
-				<div className="w-full px-3 gap-2 py-2 border shadow-sm rounded-md border-gray-200 bg-white">
+
+		<div className="w-full mb-4">
+				<div className="px-3 gap-2 py-2 shadow-sm bg-white">
 					<input className="mr-2 rounded-md" type="text" placeholder="Search" />
 					<input className="mr-2 rounded-md" type="text" placeholder="Search" />
 					<input className="mr-2 rounded-md" type="text" placeholder="Search" />
@@ -71,114 +71,147 @@ export default function Dashboard({ auth }) {
 					<Checkbox color="purple" defaultChecked />
 					<Checkbox color="pink" defaultChecked />
 				</div>
-        </div>
 
+				<div className="grid px-4 py-4 xl:grid-cols-3 2xl:grid-cols-6 gap-4">
+						<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
 
-            <div className="grid xl:grid-cols-3 2xl:grid-cols-6 gap-4">
-                <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-				<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
 
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+					
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
 
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+							<a href="#">
+								<img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
+							</a>
+						<div class="px-5 pb-5">
+							<a href="#">
+								<h3 class="text-gray-900 font-light text-[18px] tracking-tighter dark:text-white">SANFORD 300 ML</h3>
+							</a>
+							<div class="flex items-center justify-between">
+								<span class="text-[18px] font-semibold text-gray-900 dark:text-white">Rp100.000</span>
+								<span class="text-sm font-bold text-gray-900 dark:text-white">Qty: 30</span>
+							</div>
+						</div>
+					</div>
+
 				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-		<a href="#">
-        <img src={noimage} alt="Utrans logo" className="block ml-auto mr-auto"/>
-        </a>
-			<div class="px-5 pb-5">
-				<a href="#">
-					<h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Keran 2G22</h3>
-				</a>
-				<div class="flex items-center justify-between">
-					<span class="text-md font-bold text-gray-900 dark:text-white">Rp100.000</span>
-					<span class="text-sm font-bold text-gray-900 dark:text-white">Qty : 30</span>
-				</div>
-			</div>
-	</div>
-            </div>
-        </div>
-        
+
+				
+        	</div>
 
         <div className="w-80 bg-white p-3">
             <h2 className="text-lg font-bold mb-4">Selected Items</h2>
